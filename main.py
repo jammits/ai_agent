dae5711bb7bb3311cb53afb0bda25ddd06857174
jammit <james.mitsumasu@gmail.com>
2025-09-15 23:32:20 -0500

    add file to gitignore and corrected use of .usage_metadata

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+
 def main():
     print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    
+    client = genai.Client(api_key=api_key)
+   
+    try:
+        input = sys.argv[1]
 
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", contents=input
+        )
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    except IndexError:
+            sys.exit("No prompt given. Exiting program")
 
 if __name__ == "__main__":
-    main()
+    main() 
